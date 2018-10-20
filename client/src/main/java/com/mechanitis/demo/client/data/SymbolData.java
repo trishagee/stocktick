0194ec8e85ec80b8acee08fc43a355cbf0500891
Trisha Gee <trisha.gee@gmail.com>
2018-10-20 12:50:49 -0700

    This doesn't work because the collection passed into the observableList factory method is copied.  This might be the problem with my updates to the stats

--- client/src/main/java/com/mechanitis/demo/client/data/SymbolData.java
+++ client/src/main/java/com/mechanitis/demo/client/data/SymbolData.java
@@ -3,13 +3,19 @@ package com.mechanitis.demo.client.data;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
 
+import java.util.*;
 import java.util.function.Consumer;
 
 import static javafx.application.Platform.runLater;
 
 public class SymbolData implements Consumer<String> {
-    private final ObservableList<XYChart.Data<String, Number>> data = FXCollections.observableArrayList();
+    //makes sense for this to be an array actually, since I don't want to change
+    //the length I want to replace the items
+    //although... actually that's not true because I need to add to the end and remove from the front
+    private final Queue data = new ArrayDeque<Data<String, Number>>(30);
+    private final ObservableList<Data<String, Number>> observableData = FXCollections.observableArrayList(data);
     private final StockStats stockStats;
     private long tick = 0;
     private double maxPrice = 0;
@@ -23,14 +29,14 @@ public class SymbolData implements Consumer<String> {
         System.out.println("price = [" + message + "]");
         Double price = Double.valueOf(message);
         stockStats.update(price);
-//        stockStats.setMaxPrice(tick);
+        System.out.println("data.size() = " + data.size());
         runLater(() -> {
-            data.add(new XYChart.Data<>(String.valueOf(tick++), price));
+            data.add(new Data<String, Number>(String.valueOf(tick++), price));
         });
     }
 
-    public ObservableList<XYChart.Data<String, Number>> getData() {
-        return data;
+    public ObservableList<Data<String, Number>> getData() {
+        return observableData;
     }
 
 }
